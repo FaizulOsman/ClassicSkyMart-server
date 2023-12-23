08848c852878b24004d91f97ace00db61895448b
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-23 11:30:56 +0600

    feat: user registration & login functionality increment

---
+++ src/app/modules/user/user.constants.ts
@@ -0,0 +1,18 @@
+export const role = ['user', 'admin', 'super_admin'];
+
+export const UserSearchableFields = [
+  'role',
+  'phone',
+  'name',
+  'email',
+  'address',
+];
+
+export const UserFilterableFields = [
+  'searchTerm',
+  'role',
+  'phone',
+  'name',
+  'email',
+  'address',
+];
--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import { AuthRouter } from '../modules/auth/auth.router';
+import { UserRoutes } from '../modules/user/user.router';
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/',
-    route: '',
+    path: '/auth',
+    route: AuthRouter,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
   },
 ];
 
